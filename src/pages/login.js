163352da5f8721c7240e10fcfab71d60b0c9adb3
Tasnimwheebi <tasnimwheebi@gmail.com>
2021-07-21 19:53:02 +0300

    login-register

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Facebook from '../component/facebook/facebook';
 import { Link } from 'react-router-dom';
-import LogIn from '../component/login/login';
+import LogIn from '../component/login/login.js';
+import Home from './home';
 
 const LogInPage = (props) =>{
     return (
         <>
           <LogIn/>
           <h2>OR</h2>
-           <Facebook/>
-         <Link to = '/register'>
-           <button>Register</button></Link>
+           {/* <Facebook/> */}
+           <Link to='/home'>
+               homepage
+           </Link>
+           
+        
         </>
     )
 }
